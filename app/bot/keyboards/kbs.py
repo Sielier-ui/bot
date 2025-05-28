038bfbce46724fa09a08a630cb96bf3d68328249
my_name <alievaroxxxana@mail.ru>
2025-05-28 16:40:30 +0300

    first commit

---
+++ app/bot/keyboards/kbs.py
@@ -0,0 +1,14 @@
+from aiogram.types import WebAppInfo, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from app.config import settings
+
+#кнопки в интерфейсе
+
+def main_keyboard() -> InlineKeyboardMarkup: 
+    generate_url = settings.BASE_SITE
+    kb = InlineKeyboardBuilder()
+    kb.button(text="📷 Сканировать", web_app=WebAppInfo(url=f"{generate_url}/scan"))
+    kb.button(text="📤 Загрузить QR", web_app=WebAppInfo(url=f"{generate_url}/upload"))
+    kb.button(text="✨ Создать QR", web_app=WebAppInfo(url=f"{generate_url}"))
+    kb.adjust(1)
+    return kb.as_markup()
